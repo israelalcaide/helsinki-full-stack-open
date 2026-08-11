896fcdeb48103b48f6fff8ac62417e16a9eb38f8
Israel Lopez <100045658+israelalcaide@users.noreply.github.com>
2026-08-11 18:47:17 +0200

    Complete exercisse 1.11 unicafe part 6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -11,9 +11,10 @@ const Button = (props) => {
 
 const StatisticLine = (props) => {
 	return (
-		<p>
-			{props.text} {props.value}
-		</p>
+		<tr>
+			<td>{props.text}</td>
+			<td>{props.value}</td>
+		</tr>
 	)
 }
 
@@ -25,14 +26,17 @@ const Statistics = (props) => {
 	}
 
 	return (
-		<div>
-			<StatisticLine text="good" value={props.good} />
-			<StatisticLine text="neutral" value={props.neutral} />
-			<StatisticLine text="bad" value={props.bad} />
-			<StatisticLine text="all" value={total} />
-			<StatisticLine text="average" value={(props.good - props.bad) / total} />
-			<StatisticLine text="positive" value={props.good / total * 100 + " %"} />
-		</div>
+		<table>
+			<tbody>
+				<StatisticLine text="good" value={props.good} />
+				<StatisticLine text="neutral" value={props.neutral} />
+				<StatisticLine text="bad" value={props.bad} />
+				<StatisticLine text="all" value={total} />
+				<StatisticLine text="average" value={(props.good - props.bad) / total} />
+				<StatisticLine text="positive" value={props.good / total * 100 + " %"} />
+			</tbody>
+		</table>
+
 	)
 }
 
