b71e4e592d424e2702fbaa4ba075c9cffd959bdb
Israel Lopez <100045658+israelalcaide@users.noreply.github.com>
2026-08-11 13:07:29 +0200

    Complete exercise 1.10 unicafe step 5

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,22 +1,37 @@
 import { useState } from "react";
 
+
+const Button = (props) => {
+	return (
+		<button onClick={props.handleClick}>
+			{props.text}
+		</button>
+	)
+}
+
+const StatisticLine = (props) => {
+	return (
+		<p>
+			{props.text} {props.value}
+		</p>
+	)
+}
+
 const Statistics = (props) => {
 	const total = props.good + props.neutral + props.bad;
 
 	if (total === 0) {
-		return (
-			<p>No feedback given</p>
-		)
+		return (<p>No feedback given</p>)
 	}
 
 	return (
-		<div>			
-			<p>good {props.good}</p>
-			<p>neutral {props.neutral}</p>
-			<p>bad {props.bad}</p>
-			<p>all {props.good + props.neutral + props.bad}</p>
-			<p>average {(props.good - props.bad) / (props.good + props.neutral + props.bad)}</p>
-			<p>positive {props.good / (props.good + props.neutral + props.bad) * 100} %</p>
+		<div>
+			<StatisticLine text="good" value={props.good} />
+			<StatisticLine text="neutral" value={props.neutral} />
+			<StatisticLine text="bad" value={props.bad} />
+			<StatisticLine text="all" value={total} />
+			<StatisticLine text="average" value={(props.good - props.bad) / total} />
+			<StatisticLine text="positive" value={props.good / total * 100 + " %"} />
 		</div>
 	)
 }
@@ -30,9 +45,9 @@ const App = () => {
 	return (
 		<div>
 			<h1>give feedback</h1>
-			<button onClick={() => setGood(good + 1)}>good</button>
-			<button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-			<button onClick={() => setBad(bad + 1)}>bad</button>
+			<Button text="good" handleClick={() => setGood(good + 1)} />
+			<Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
+			<Button text="bad" handleClick={() => setBad(bad + 1)} />
 			<h1>statistics</h1>
 			<Statistics good={good}
 				neutral={neutral}
