916ea4ccb5366b314d1ce608268213b79f96d2bc
Israel Lopez <100045658+israelalcaide@users.noreply.github.com>
2026-08-15 19:41:15 +0200

    Complete exercise 2.2 courseinfo

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -20,41 +20,54 @@ const Content = (props) => {
 	)
 }
 
+const Total = (props) => {
+	return (
+		<p>
+			total of 
+			{' '}
+			{props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+			{' '}
+			exercises
+		</p>
+	)
+}
+
 
 const Course = (props) => {
 	return (
 		<div>
 			<Header course={props.course.name} />
 			<Content parts={props.course.parts} />
+			<Total parts={props.course.parts} />
 		</div>
 	)
 
 }
 
 const App = () => {
-	const course = {
-		id: 1,
-		name: 'Half Stack application development',
-		parts: [
-			{
-				name: 'Fundamentals of React',
-				exercises: 10,
-				id: 1
-			},
-			{
-				name: 'Using props to pass data',
-				exercises: 7,
-				id: 2
-			},
-			{
-				name: 'State of a component',
-				exercises: 14,
-				id: 3
-			}
-		]
-	}
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
 
-	return <Course course={course} />
+  return <Course course={course} />
 }
 
 export default App
