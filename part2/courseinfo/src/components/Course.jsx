c37a3aac718ba871e52a4b515b9be37d05816a69
Israel Lopez <100045658+israelalcaide@users.noreply.github.com>
2026-08-26 14:55:47 +0200

    Complete exercise 2.5 courseinfo

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,44 @@
+const Header = (props) => {
+	return (
+		<h1>{props.course}</h1>
+	)
+}
+const Part = (props) => {
+	return (
+		<p>
+			{props.part} <b>{props.exercises}</b>
+		</p>
+	)
+}
+
+const Content = (props) => {
+	return (
+		<div>
+			{props.parts.map((part) => <Part key={part.id} part={part.name} exercises={part.exercises} />)}
+		</div>
+	)
+}
+
+const Total = (props) => {
+	const total = props.parts.reduce((sum, part) => {
+		return (sum + part.exercises)
+	}, 0)
+	return (
+		<p>
+			total of <b>{total}</b> exercises
+		</p>
+	)
+}
+
+const Course = (props) => {
+	return (
+		<div>
+			<Header course={props.course.name} />
+			<Content parts={props.course.parts} />
+			<Total parts={props.course.parts} />
+		</div>
+	)
+
+}
+
+export default Course
