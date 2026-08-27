a473377f1905b2f3ac9f2b7e7e72e591fc31af19
Israel Lopez <100045658+israelalcaide@users.noreply.github.com>
2026-08-27 18:38:51 +0200

    Complete exercise 2.7 phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -18,8 +18,13 @@ const App = () => {
 			name: newName
 		}
 
-		setPersons(persons.concat(personObject))
-
+		if(persons.some(person => person.name === newName)) {
+			alert(`${newName} is already added to phonebook`)
+		}
+		else { 
+			setPersons(persons.concat(personObject))
+		}
+		
 		setNewName('')
 	}
 
