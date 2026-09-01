b5e7e484a69a935b9ba4d283acd1741fbbfbbe29
Israel Lopez <100045658+israelalcaide@users.noreply.github.com>
2026-09-01 20:54:17 +0200

    Complete exercise 2.11 phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
 const Filter = (props) => {
@@ -37,16 +38,11 @@ const Persons = (props) => {
 
 
 const App = () => {
-	const [persons, setPersons] = useState([
-		{ name: 'Arto Hellas', number: '040-123456', id: 1 },
-		{ name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-		{ name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-		{ name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-	])
-
+	const [persons, setPersons] = useState([])
 	const [newName, setNewName] = useState('')
 	const [newNumber, setNewNumber] = useState('')
 	const [filter, setFilter] = useState('')
+
 
 	const handleNameChange = (event) => {
 		setNewName(event.target.value)
@@ -60,6 +56,14 @@ const App = () => {
 		setFilter(event.target.value)
 	}
 
+	useEffect(() => {
+		const promise = axios.get('http://localhost:3001/persons')
+
+		promise.then(response => {
+			setPersons(response.data)
+		})
+
+	}, [])
 
 	const addPerson = (event) => {
 		event.preventDefault()
